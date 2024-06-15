57baeb9f9d2b417ffafc6abbe7f02725648f6469
Gabriel Aguiar <lugafeagre@gmail.com>
2024-06-15 18:43:39 -0300

    Fix password in user model

--- server/ERPserver/main_app/serializers.py
+++ server/ERPserver/main_app/serializers.py
@@ -1,11 +1,37 @@
 from rest_framework import serializers
 from .models import User, Supply, Sale, Inventory, Product
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
         fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name']
 
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        password = validated_data.get('password', None)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
 class SupplySerializer(serializers.ModelSerializer):
     class Meta:
         model = Supply
