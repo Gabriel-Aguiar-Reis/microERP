3c33f5b3672b333cc0ecd6ed9af59987bb967bf2
Gabriel Aguiar <lugafeagre@gmail.com>
2024-06-15 18:33:31 -0300

    Create models

---
+++ server/ERPserver/main_app/models.py
@@ -0,0 +1,46 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+
+class Supply(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    commercial_id = models.CharField(max_length=100, blank=False, editable=True, unique=True)
+    supply_date = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'<{self.commercial_id}> {self.supply_date}'
+
+class Sale(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False, related_name='sales')
+    sale_date = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'<{self.seller.get_full_name()}> {self.sale_date}'
+    
+class Inventory(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=255, unique=True, blank=False, editable=True)
+    description = models.TextField(blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False, related_name='inventories')
+    
+    def __str__(self):
+        return f'<{self.name}> {self.owner}'
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    commercial_id = models.CharField(max_length=100, blank=False, editable=True, unique=True)
+    supply = models.ForeignKey(Supply, on_delete=models.CASCADE, blank=False, null=False, related_name='products')
+    sales = models.ManyToManyField(Sale, blank=True, related_name='products')
+    name = models.CharField(max_length=255, unique=True, blank=False, editable=True)
+    description = models.TextField(blank=True)
+    cost_price = models.FloatField(default=0, blank=True, editable=True)
+    sell_price = models.FloatField(default=0, blank=True, editable=True)
+    units = models.IntegerField(default=0, blank=True, editable=True)
+    inventory = models.ForeignKey(Inventory, on_delete=models.CASCADE, blank=False, null=False, related_name='products')
+    
+    def __str__(self):
+        return f'<{self.name}>'
