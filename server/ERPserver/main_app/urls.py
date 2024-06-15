c1a1a3abf3c97c21b99815be367fba6d73093a44
Gabriel Aguiar <lugafeagre@gmail.com>
2024-06-15 18:41:06 -0300

    Create urls from main_app

---
+++ server/ERPserver/main_app/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from .views import (
+    UserListCreateView, UserDetailView,
+    SupplyListCreateView, SupplyDetailView,
+    SaleListCreateView, SaleDetailView,
+    InventoryListCreateView, InventoryDetailView,
+    ProductListCreateView, ProductDetailView
+)
+
+urlpatterns = [
+    path('users/', UserListCreateView.as_view(), name='user-list-create'),
+    path('users/<uuid:pk>/', UserDetailView.as_view(), name='user-detail'),
+    path('supplies/', SupplyListCreateView.as_view(), name='supply-list-create'),
+    path('supplies/<uuid:pk>/', SupplyDetailView.as_view(), name='supply-detail'),
+    path('sales/', SaleListCreateView.as_view(), name='sale-list-create'),
+    path('sales/<uuid:pk>/', SaleDetailView.as_view(), name='sale-detail'),
+    path('inventories/', InventoryListCreateView.as_view(), name='inventory-list-create'),
+    path('inventories/<uuid:pk>/', InventoryDetailView.as_view(), name='inventory-detail'),
+    path('products/', ProductListCreateView.as_view(), name='product-list-create'),
+    path('products/<uuid:pk>/', ProductDetailView.as_view(), name='product-detail'),
+]
