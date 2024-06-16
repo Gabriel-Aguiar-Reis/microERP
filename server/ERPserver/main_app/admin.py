3b9566d601b7930eb510d5b3a07d76f768b40b19
Gabriel Aguiar <lugafeagre@gmail.com>
2024-06-15 21:19:29 -0300

    Register models in admin

--- server/ERPserver/main_app/admin.py
+++ server/ERPserver/main_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Inventory, Product, Sale, Supply, User
+
+admin.site.register([Inventory, Product, Sale, Supply, User])
