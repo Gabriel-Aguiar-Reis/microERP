6e74ea0bb181691e0fe4a3718a5a7ee284f39c75
Gabriel Aguiar <lugafeagre@gmail.com>
2024-06-16 23:39:36 -0300

    Fix build.sh bug

--- server/build.sh
+++ server/build.sh
@@ -7,12 +7,10 @@ poetry install
 
 cd ERPserver
 
+python manage.py collectstatic --no-input
+python manage.py makemigrations
+python manage.py migrate
 if [[ $CREATE_SUPERUSER ]];
 then
   python manage.py createsuperuser --no-input
 fi
-
-python manage.py collectstatic --no-input
-python manage.py makemigrations
-python manage.py migrate
-python manage.py showmigrations
