64192126a1fcde1f8e85041cfa2bc53cfab0ad8f
Gabriel Aguiar <lugafeagre@gmail.com>
2024-06-16 23:34:23 -0300

    Fix superuser creation on Render

--- server/build.sh
+++ server/build.sh
@@ -7,6 +7,11 @@ poetry install
 
 cd ERPserver
 
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
+
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
