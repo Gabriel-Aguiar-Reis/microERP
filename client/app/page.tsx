9839725dbfa279a6b9f11a1036bcf30de9c4ea7a
Gabriel Aguiar <lugafeagre@gmail.com>
2024-08-31 15:19:47 -0300

    fix client routing to development

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,21 @@
 "use client"
 
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter()
+
   return (
-    <p>Hello World</p>
+    <div className="w-64 flex justify-between">
+      <button type="button" onClick={() => router.push('/login')}>
+        Login
+      </button>
+      <button type="button" onClick={() => router.push('/products/all-products')}>
+        Produtos
+      </button>
+      <button type="button" onClick={() => router.push('/sales/recent-sales')}>
+        Vendas
+      </button>
+    </div>
   );
 }
