01ac9af19c7208a2d751609c6f83390b1b6c8199
Gabriel Aguiar <lugafeagre@gmail.com>
2024-08-30 23:14:22 -0300

    add highlight to selected link in aside navbar

--- client/components/blocks/recent-sales.tsx
+++ client/components/blocks/recent-sales.tsx
@@ -101,7 +101,7 @@ export function RecentSales() {
               <TooltipTrigger asChild>
                 <Link
                   href="#"
-                  className="flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                  className="flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8  bg-slate-200"
                 >
                   <ShoppingCart className="h-5 w-5" />
                   <span className="sr-only">Vendas</span>
