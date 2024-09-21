83181e0fe3530b30f54e41f7e0c7c8d157202175
Gabriel Aguiar <lugafeagre@gmail.com>
2024-09-21 14:48:15 -0300

    fix response interceptors redirection bug

--- client/lib/axios.ts
+++ client/lib/axios.ts
@@ -21,18 +21,24 @@ api.interceptors.request.use(
 
 // Function to refresh the access token
 async function refreshAccessToken() {
-  const refreshToken = localStorage.getItem('refreshToken')
-  if (!refreshToken) {
-    throw new Error('No refresh token available')
+  try {
+    const refreshToken = localStorage.getItem('refreshToken')
+    if (!refreshToken) {
+      throw new Error('No refresh token available')
+    }
+    const response = await api.post('/token/refresh/', {
+      refresh: refreshToken
+    })
+    const newAccessToken = response.data.access
+    const newRefreshToken = response.data.refresh
+
+    localStorage.setItem('accessToken', newAccessToken)
+    localStorage.setItem('refreshToken', newRefreshToken)
+
+    return newAccessToken
+  } catch (e) {
+    throw e
   }
-  const response = await api.post('/token/refresh/', { refresh: refreshToken })
-  const newAccessToken = response.data.access
-  const newRefreshToken = response.data.refresh
-
-  localStorage.setItem('accessToken', newAccessToken)
-  localStorage.setItem('refreshToken', newRefreshToken)
-
-  return newAccessToken
 }
 
 // Response interceptor to handle 401 errors and retry requests
@@ -44,12 +50,11 @@ api.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config
 
-    // Check if the error status is 401 (Unauthorized)
+    // Se o erro é 401 e não está tentando já o refresh
     if (error.response?.status === 401 && !originalRequest._retry) {
-      // Set retry flag to prevent infinite loops
       originalRequest._retry = true
 
-      // If a token refresh is already in progress, queue the failed request
+      // Se já está atualizando o token, enfileirar a requisição
       if (isRefreshing) {
         return new Promise((resolve, reject) => {
           failedRequestsQueue.push({ resolve, reject })
@@ -63,35 +68,36 @@ api.interceptors.response.use(
 
       isRefreshing = true
 
-      // Try refreshing the token
-      return new Promise(async (resolve, reject) => {
-        try {
-          const newAccessToken = await refreshAccessToken()
-          originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`
+      try {
+        // Tentando renovar o access token
+        const newAccessToken = await refreshAccessToken()
 
-          // Retry all queued requests with the new token
-          failedRequestsQueue.forEach((req) => req.resolve(newAccessToken))
-          failedRequestsQueue = []
+        // Atualizando headers e reenviando requisições
+        failedRequestsQueue.forEach((req) => req.resolve(newAccessToken))
+        failedRequestsQueue = []
 
-          resolve(api(originalRequest)) // Retry the failed request
-        } catch (err) {
-          failedRequestsQueue.forEach((req) => req.reject(err))
-          failedRequestsQueue = []
+        originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`
+        return api(originalRequest) // Retentar a requisição original
+      } catch (err) {
+        // Se a renovação falhar, limpar tokens e redirecionar para login
+        failedRequestsQueue.forEach((req) => req.reject(err))
+        failedRequestsQueue = []
 
-          // Redirect to login if refreshing the token fails
-          localStorage.removeItem('accessToken')
-          localStorage.removeItem('refreshToken')
-          if (!(window.location.href === 'http://localhost:3000/login')) {
-            window.location.href = '/login'
-          }
-          reject(err)
-        } finally {
-          isRefreshing = false
+        localStorage.removeItem('accessToken')
+        localStorage.removeItem('refreshToken')
+
+        // Redirecionar para o login
+        if (window.location.pathname !== '/login') {
+          window.location.href = '/login'
         }
-      })
+
+        return Promise.reject(err)
+      } finally {
+        isRefreshing = false
+      }
     }
 
-    return Promise.reject(error) // Reject all other errors
+    return Promise.reject(error) // Rejeitar todos os outros erros
   }
 )
 
