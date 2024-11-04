99a2ab939606aa1523301f9d1a760425add713bb
Gabriel Aguiar <lugafeagre@gmail.com>
2024-11-04 17:24:16 -0300

    fix build.sh

--- server/build.sh
+++ server/build.sh
@@ -13,4 +13,7 @@ python manage.py makemigrations
 
 python manage.py migrate
 
-python manage.py createsuperuser --noinput
+if [[ $CREATE_SUPERUSER ]];
+then
+    python manage.py createsuperuser --no-input
+fi
